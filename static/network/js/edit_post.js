881428a81de199aa31c5c610d6b7fe65583149d8
WildLotus-15 <glasssesmr@gmail.com>
2022-04-30 15:42:32 +0400

    IT WORKS!!

--- static/network/js/edit_post.js
+++ static/network/js/edit_post.js
@@ -13,23 +13,67 @@ function manage_post_edit(post_id) {
 
     const save_button = document.querySelector(`#post_save_button_${post_id}`)
 
+    const by_default_visibility = document.querySelector(`#post_edit_form__${post_id}`).dataset.default_visibility_default
+
     save_button.disabled = true
 
-    edit_input.addEventListener('keyup', (event) => manage_post_update(event, save_button, edit_input.dataset.default_value))
-    edit_input.addEventListener('keydown', (event) => manage_post_update(event, save_button, edit_input.dataset.default_value))
+    const form_group = document.querySelector(`#post_${post_id}_form_group`)
+
+    let last_selected_radio = document.createElement('input')
+    last_selected_radio.type = "hidden"
+    last_selected_radio.id = `hidden_input_${post_id}`
+    last_selected_radio.dataset.by_default_visibility = by_default_visibility
+    console.log(last_selected_radio)
+
+    form_group.onchange = (e) => {
+        last_selected_radio.dataset.by_default_visibility = e.target.dataset.visibility
+        console.log(last_selected_radio)
+
+        if (!(by_default_visibility == e.target.dataset.visibility) || edit_input.value !== edit_input.dataset.default_value) {
+            if (edit_input.value.length > 0) {
+                save_button.disabled = false
+            }
+        } else {
+            save_button.disabled = true
+        }
+    }
+
+    edit_input.addEventListener('keyup', (event) => manage_post_update(event, save_button, edit_input.dataset.default_value, post_id, edit_input, last_selected_radio, by_default_visibility))
     edit_input.addEventListener('paste', (event) => manage_post_paste(event, save_button, edit_input.dataset.default_value, edit_input))
 
     save_button.addEventListener('click', () => save_changes(post_id, edit_input.value))
 }
 
-function manage_post_update(event, save_button, default_value) {
+function manage_post_update(event, save_button, default_value, post_id, edit_input, last_selected_radio, by_default_visibility) {
     const new_value = event.target.value
 
-    if (new_value !== default_value && new_value.length > 0) {
+
+    const edit_only_me = document.querySelector(`#edit_only_me_${post_id}`)
+    const edit_only_friends = document.querySelector(`#edit_only_friends_${post_id}`)
+
+    // console.log((edit_only_friends.checked && edit_input.dataset.default_visibility == "only_friends" && new_value !== default_value) || (edit_only_me.checked && edit_input.dataset.default_visibility == "only_me" && new_value !== default_value))
+
+    /* if ((edit_only_friends.checked && edit_input.dataset.default_visibility == "only_friends" || edit_only_me.checked && edit_input.dataset.default_visibility == "only_me") && new_value !== default_value) {
+        if (new_value !== default_value && new_value.length > 0) {
+            console.log('A')
+        }
+    } */
+
+    /* document.querySelectorAll(`.edit_post_radio_${post_id}`).forEach(radio => {
+        console.log(radio.hasAttribute("data-default_visibility"))
+        if (radio.hasAttribute("data-default_visibility") && new_value !== default_value && new_value.length > 0) {
+            save_button.disabled = true
+        } else {
+            save_button.disabled = false
+        }
+    }) */
+
+    console.log((new_value !== default_value || last_selected_radio.dataset.by_default_visibility !== by_default_visibility) && new_value.length > 0)
+    if ((new_value !== default_value || last_selected_radio.dataset.by_default_visibility !== by_default_visibility) && new_value.length > 0) {
         save_button.disabled = false
     } else {
         save_button.disabled = true
-    }
+    } 
 }
 
 function manage_post_paste(event, save_button, default_value, edit_input) {
@@ -47,8 +91,8 @@ function manage_post_paste(event, save_button, default_value, edit_input) {
 }
 
 function save_changes(post_id, description) {
-    const only_friends = document.querySelector('#only_friends').checked
-    const only_me = document.querySelector('#only_me').checked
+    const only_friends = document.querySelector(`#edit_only_friends_${post_id}`).checked
+    const only_me = document.querySelector(`#edit_only_me_${post_id}`).checked
 
     fetch(`/post/${post_id}/edit`, {
         method: "PUT",
@@ -61,11 +105,10 @@ function save_changes(post_id, description) {
             "only_me": only_me
         })
     })
-    .then(response => response.json())
-    .then(response => {
-        console.log(response.message)
+        .then(response => response.json())
+        .then(response => {
+            console.log(response.message)
 
-        document.querySelector(`#post_description_${post_id}`).innerHTML = description
-        document.querySelector(`#close_modal_${post_id}`).click()
-    })
+            window.location.reload()
+        })
 }
